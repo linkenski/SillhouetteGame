d389509fb8f8430f986984b1506ae271605b5a63
linkenski <linkenski@gmail.com>
2018-03-27 14:42:05 +0200

    I've probably screwed it up for y'all but it works on my end

--- Source/SillhouetteGame/CollisionPlayer.cpp
+++ Source/SillhouetteGame/CollisionPlayer.cpp
@@ -1,47 +1,47 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "CollisionPlayer.h"
-#include "BasicClasses.h"
+//#include "CollisionPlayer.h"
+//#include "BasicClasses.h"
 
 
-// Sets default values for this component's properties
-UCollisionPlayer::UCollisionPlayer()
-{
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
-}
-
-
-// Called when the game starts
-void UCollisionPlayer::BeginPlay()
-{
-	Super::BeginPlay();
-
-	// ...
-	
-	startPosition = GetOwner()->GetActorLocation;
-}
-
-
-// Called every frame
-void UCollisionPlayer::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
-{
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-	// ...
-
-	AActor* owner = GetOwner();	owner->FindComponentByClass<U>();
-}
-
-
-void UCollisionPlayer::OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor,
-	class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
-{
-	if (OtherActor->ActorHasTag("hazardous"))
-	{
-		GetOwner()->GetActorTransform().SetLocation(startPosition);
-	}
-}
+//// Sets default values for this component's properties
+//UCollisionPlayer::UCollisionPlayer()
+//{
+//	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+//	// off to improve performance if you don't need them.
+//	PrimaryComponentTick.bCanEverTick = true;
+//
+//	// ...
+//}
+//
+//
+//// Called when the game starts
+//void UCollisionPlayer::BeginPlay()
+//{
+//	Super::BeginPlay();
+//
+//	// ...
+//	
+//	startPosition = GetOwner()->GetActorLocation;
+//}
+//
+//
+//// Called every frame
+//void UCollisionPlayer::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+//{
+//	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+//
+//	// ...
+//
+//	AActor* owner = GetOwner();//	owner->FindComponentByClass<U>();
+//}
+//
+//
+//void UCollisionPlayer::OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor,
+//	class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+//{
+//	if (OtherActor->ActorHasTag("hazardous"))
+//	{
+//		GetOwner()->GetActorTransform().SetLocation(startPosition);
+//	}
+//}
--- Source/SillhouetteGame/CollisionPlayer.h
+++ Source/SillhouetteGame/CollisionPlayer.h
@@ -1,32 +1,32 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#pragma once
-
-#include "Engine/World.h"
-#include "CoreMinimal.h"
-#include "Components/ActorComponent.h"
-#include "CollisionPlayer.generated.h"
-
-
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
-class SILLHOUETTEGAME_API UCollisionPlayer : public UActorComponent
-{
-	GENERATED_BODY()
-
-public:	
-	// Sets default values for this component's properties
-	UCollisionPlayer();
-
-protected:
-	// Called when the game starts
-	virtual void BeginPlay() override;
-	FVector startPosition;
-
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-	void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
-
-		
-	
-};
+//// Fill out your copyright notice in the Description page of Project Settings.
+//
+//#pragma once
+//
+//#include "Engine/World.h"
+//#include "CoreMinimal.h"
+//#include "Components/ActorComponent.h"
+//#include "CollisionPlayer.generated.h"
+//
+//
+//UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+//class SILLHOUETTEGAME_API UCollisionPlayer : public UActorComponent
+//{
+//	GENERATED_BODY()
+//
+//public:	
+//	// Sets default values for this component's properties
+//	UCollisionPlayer();
+//
+//protected:
+//	// Called when the game starts
+//	virtual void BeginPlay() override;
+//	FVector startPosition;
+//
+//public:	
+//	// Called every frame
+//	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+//	void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+//
+//		
+//	
+//};
